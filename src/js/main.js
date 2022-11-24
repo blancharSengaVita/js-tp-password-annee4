5760b7ab7a5bc413af5322835f3d428f13417d7c
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-11-24 12:02:39 +0100

    correction prof

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,36 @@
-const togglePassword = {
-  elementCible(){
-    document.documentElement.classList.add('js-enabled');
-     this.input = document.getElementById('password');
-     this.button = document.querySelector('.showPass');
-  },
+document.documentElement.classList.add('js-enabled');
 
-  toggle(){
-    this.button.addEventListener('click', ()=>{
-      this.input.type === 'password' ? this.input.type = 'text': this.input.type = 'password';
+/*const input = document.getElementById('password');
+const button = document.querySelector('.showPass');*/
+
+/*button.addEventListener('click', (e)=>{
+  [input.type,input.dataset.type] = [input.dataset.type,input.type];
+  [button.dataset.text, button.textContent] = [button.textContent, button.dataset.text];
+});
+
+input.focus(); //il fonctionne pas*/
+
+const showPassword = {
+  input : document.getElementById('password'),
+  button : document.querySelector('.showPass'),
+  init() {
+    this.input.focus();
+    this.button.addEventListener('click', (e)=>{
+      [this.input.type,this.input.dataset.type] = [this.input.dataset.type,this.input.type];
+      [this.button.dataset.text, this.button.textContent] = [this.button.textContent, this.button.dataset.text];
     });
-  },
+  }
+};
+object.init();
 
-  init(){
-    this.elementCible();
-    this.toggle();
-  },
-}
 
-togglePassword.init();
+
+
+
+
+
+
+
+
+
+//le hot reload ne change pas la page mais change la partie qu'on vient de mettre à jour dans la mémoire. le open in browser dans phpstorm ne permet pas de faire les choses qu'on vu qu'on est capable de faire avec npm
